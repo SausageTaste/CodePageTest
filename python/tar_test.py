be54dc09b39e4715257a1f28116208754a5f2a7e
Sungmin Woo <woos8899@gmail.com>
2026-01-10 19:57:59 +0900

    Update test script

--- python/tar_test.py
+++ python/tar_test.py
@@ -7,7 +7,7 @@ def __remove_all_extracted_folders():
     for x in os.listdir():
         if not os.path.isdir(x):
             continue
-        if not x.startswith("utf-8-paths"):
+        if not x.startswith("test-"):
             continue
         shutil.rmtree(x)
 
@@ -22,15 +22,25 @@ def main():
 
     repo_root_dir = pathlib.Path(__file__).parent.parent.resolve()
     test_tar_path = repo_root_dir / "data" / "utf-8-paths.tar.gz"
+    custom_cmake_path = repo_root_dir / "build" / "submodules" / "cmake" / "bin" / "cmake.exe"
 
-    os.system(f"cmake -E tar xv \"{test_tar_path}\"")
-    __try_renaming("utf-8-paths", "utf-8-paths-global-cmake")
+    print("-" * 80)
+    os.system("cmake --version")
+    result = os.system(f"cmake -E tar xvf \"{test_tar_path}\"")
+    __try_renaming("utf-8-paths", "test-utf-8-paths-global-cmake")
+    print(f"Result: {result}")
 
-    os.system(f"tar -xvf \"{test_tar_path}\"")
-    __try_renaming("utf-8-paths", "utf-8-paths-global-tar")
+    print("-" * 80)
+    os.system("tar --version")
+    result = os.system(f"tar -xvf \"{test_tar_path}\"")
+    __try_renaming("utf-8-paths", "test-utf-8-paths-global-tar")
+    print(f"Result: {result}")
 
-    os.system(f"bsdtar -xvf \"{test_tar_path}\"")
-    __try_renaming("utf-8-paths", "utf-8-paths-global-bsdtar")
+    print("-" * 80)
+    os.system(f"{custom_cmake_path} --version")
+    result = os.system(f"{custom_cmake_path} -E tar xvf \"{test_tar_path}\"")
+    __try_renaming("utf-8-paths", "test-utf-8-paths-custom-cmake")
+    print(f"Result: {result}")
 
 
 if __name__ == "__main__":
